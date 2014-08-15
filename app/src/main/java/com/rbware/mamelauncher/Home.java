a1dad71f28f80e9553ea1239970e6f6e73025637
Ryan Bailey <rbwaredev@gmail.com>
2014-08-15 13:31:28 -0500

    [RB] Project updated

--- app/src/main/java/com/rbware/mamelauncher/Home.java
+++ app/src/main/java/com/rbware/mamelauncher/Home.java
@@ -1,8 +1,12 @@
 /*
- * This file is a part of Null Launcher.
+ * This file is a part of Mame Launcher.
+ * Mame Launcher is a fork of Michael Howell's Null-Launcher
+ * Copyright 2014 Ryan Bailey <rbwaredev@gmail.com>
+ *
+ * Null-Launcher
  * Copyright 2013 Michael Howell <michael@notriddle.com>
  *
- * Null Launcher is free software: you can redistribute it and/or modify
+ * Mame Launcher is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -13,10 +17,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Null Launcher. If not, see <http://www.gnu.org/licenses/>.
+ * along with Mame Launcher. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.notriddle.null_launcer;
+package com.rbware.mamelauncher;
 
 import android.app.Activity;
 import android.content.Intent;
